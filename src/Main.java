9d2e67e3e70349772d1102b08329a0bbdbbaad9c
jakwie002 <jakwie002@utp.edu.pl>
2021-03-12 10:19:59 +0100

    zmiana1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {public static void main(String[] args)
 {
-    System.getProperty("java.version");     }
+    System.getProperty("java.version");
+    System.out.println("Main.java")}
 }
